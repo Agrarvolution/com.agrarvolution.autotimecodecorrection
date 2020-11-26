085e8f117160cf9cc0f112a8729e9a23e57e4836
Agrarvolution <s1610238028@students.fh-hagenberg.at>
2020-11-26 21:04:33 +0100

    Settings handling

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 //Define form names
 let formId = 'atc';
 let sourceId = 'source';
-let searchMethodId = 'target';
+let searchTargetId = 'target';
 let searchRecursionId = 'recursion';
 let loggingId = 'logging';
 
@@ -10,35 +10,52 @@ let verboseLogging = true;
 const logLevels = {
     critical: "CRIT",
     status: "STAT",
-    info: "INFO"
+    info: "INFO",
+    error: "ERR"
 }
 let log = null;
 let error = null;
 
-let settingsKey = "settings";
+const settingsKey = "autoTimecodeCorrection.settings";
+const defaultSettings = {
+    logging: false,
+    searchRecursive: true,
+    searchTarget: 1,
+}
+alert("Fuck you");
 
 $(document).ready(function () {
     log = $('#log');
     error = $('#errorDisplay');
 
-    loadSettings();
-    
-    $('#start').click(function(e){
+    alert("Fuck you less");
+    let settings = defaultSettings;
+    settings = loadSettings();
+    changeSettings(settings);
+    $('#reset').on(summit, function(e){
+        e.preventDefault();
+        addLog("Default settings are restored.", logLevels.info);
+        changeSettings(defaultSettings);
+        $('#source')[0].value = "";
+    });
+
+    /*$('input:not(#source)').on(change, function (e) {
+        if (storeSettings(settings)) {
+            addLog("Settings successfully stored.", logLevels.info);
+        };
+    });*/
+
+
+    $('#start').on(click, function(e){
         e.preventDefault();
         let form = document.forms[formId];
 
-        if (!validateForm(form)) {
-            addLog('Process canceled. Inputs are invalid. See logs above.', logLevels.status);
+        if (!validateForm(form,settings)) {
+            addLog('Process canceled. Inputs are invalid. See logs above.', logLevels.info);
             
         }
            
         //process form -> results = settings
-        let settings = {
-            lastFile: 0,
-            logging: form[loggingId],
-            searchMethod: form[searchMethodId],
-            searchRecursive: form[searchRecursionId]
-        }
         if (storeSettings(settings)) {
             addLog("Settings successfully stored.", logLevels.info);
         };
@@ -49,6 +66,26 @@ $(document).ready(function () {
     let testCSV = 'File Name,Duration,File TC,Audio TC,Framerate\n"NZ6_0394.MOV","00:00:05","15:21:06:07","08:22:12:18","25.00",';
 });
 
+function changeSettings(settings) {
+    try {
+        let form = $('form[name="atc"]');
+
+        form.children('#logging')[0].checked = settings.logging;
+        form.children('#recursion')[0].checked = settings.searchRecursive;
+        let radioSelection = form.children('[name="target]');
+        for (let i = 0; i <  radioSelection.length; i++) {
+            radioSelection[i].checked = false;
+            if (i === settings.searchTarget) {
+                radioSelection[i].checked = true;
+            }
+        }
+        verboseLogging = settings.logging;
+        addLog("Settings successfully updated.", logLevels.info);
+    } catch {
+        addLog("Failed to update settings.", logLevels.error);
+    }
+}
+
 function storeSettings(settings) {
     let settingsTxt = "";
     try {
@@ -57,7 +94,7 @@ function storeSettings(settings) {
         addLog("Failed to create settings string.", logLevels.critical);
         return false;
     }
-    localStorage.setItem(settingsKey, settings);
+    localStorage.setItem(settingsKey, settingsTxt);
     return true;
 }
 
@@ -65,33 +102,53 @@ function loadSettings() {
     let settings = localStorage.getItem(settingsKey);
     if (settings === null) {
         addLog("No settings have been stored.", logLevels.info);
+        settings = defaultSettings;
     } else {
         try {
             settings = JSON.parse(settings);
         } catch {
             addLog("Failed to create settings object.", logLevels.critical);
-            return false;
+            settings = defaultSettings;
         }
-        let form = $('form[name="atc"]');
-
     }
-  
+    return settings;
 }
 
-function validateForm(form) {
+function validateForm(form, input) {
     if (form[sourceId].files.length === 0) {
         addLog('No file has been selected.', logLevels.status);
         return false;
     } else if (form[sourceId].files[0].size === 0) {
         addLog('Selection is not a file.', logLevels.status);
         return false;
-    } 
+    } else if (form[sourceId].files[0].type === 'text/csv') {
+        addLog('File type does not match.', logLevels.info);
+    }
+    input.file = form[sourceId].files[0];
 
+    input.logging = form[loggingId].checked; 
+    
+    input.searchRecursive = form[searchRecursionId].checked;
+
+    let searchTargetSelection = 0;
+    for (let i = 0; i < form[searchTargetId].length; i++) {
+        if (form[searchTargetId][i].checked) {
+            searchTargetSelection++;
+            input.searchTarget = i;
+        }
+    }
+    if (searchTargetSelection != 1) {
+        addLog('Search target is not defined.', logLevels.status);
+        return false;
+    }
+    return input;
 }
 
 function addLog (text, level) {
-    error[0].value = text;
-    if (verboseLogging) {
+    if (level === logLevels.status) {
+        error.text(text);
+    }
+    if (level === logLevels.error || verboseLogging) {
         if (log.hasClass('hidden')) {
             log.removeClass('hidden');
         }   
@@ -108,6 +165,11 @@ function clearLog () {
 
 function timeStamp() {
     let date = new Date();
-    return "[" + date.getDay() + "." + date.getMonth() + "." + date.getFullYear() + " - " +
-        date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + "] ";
+    return "[" + leadingZero(date.getDate()) + "." + leadingZero(date.getMonth()+1) + "." + 
+    date.getFullYear() + " - " + leadingZero(date.getHours()) + ":" + leadingZero(date.getMinutes()) + 
+    ":" + leadingZero(date.getSeconds()) + "] ";
+}
+
+function leadingZero(number) {
+    return number < 10 ? "0" + number : number;
 }
