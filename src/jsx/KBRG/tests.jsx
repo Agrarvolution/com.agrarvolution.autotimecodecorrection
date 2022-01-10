6d4896a02e7de8df4605c933efe840d54963a279
Agrarvolution <kilian.hartinger@aon.at>
2022-01-10 01:05:43 +0100

    Disable tests

--- src/jsx/KBRG/tests.jsx
+++ src/jsx/KBRG/tests.jsx
@@ -1,9 +1,9 @@
-#include 'KBRG_TimecodeCorrection.jsx';
+//#include 'KBRG_TimecodeCorrection.jsx';
 
 
 
-var timeCorrection = $.agrarvolution.timecodeCorrection;
-timeCorrection.metaDataOfSelected();
+//var timeCorrection = $.agrarvolution.timecodeCorrection;
+//timeCorrection.metaDataOfSelected();
 //timeCorrection.cacheMediaObjects();
 //timeCorrection.processInput(tcObject);
 //timeCorrection.updateTimeCodes();
