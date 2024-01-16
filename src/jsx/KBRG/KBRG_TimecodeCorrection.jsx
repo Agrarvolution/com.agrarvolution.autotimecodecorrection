1c5428df4c59e346ffa9df4118b234df89b8ffc1
Agrarvolution <acc.github@agrarvolution.at>
2024-01-16 16:22:12 +0100

    Better visible default setting

--- src/jsx/KBRG/KBRG_TimecodeCorrection.jsx
+++ src/jsx/KBRG/KBRG_TimecodeCorrection.jsx
@@ -705,11 +705,10 @@ $.agrarvolution.timecodeCorrection = {
     /**
      * Helper function to add default framerate information to an empty file.
      */
-    setEmptyStartTimeProperty: function(mediaItem, framerate) {
+    setEmptyStartTimeProperty: function(mediaItem, framerate = 25) {
         if (mediaItem.tcStruct === '') {
             return false;
         }
-        framerate = framerate || 25; //default safety
         // mediaItem.xmp.setProperty(XMPConst.NS_DM, "startTimeScale", "12800"); //#Todo determine whether this is needed or not
         // mediaItem.xmp.setProperty(XMPConst.NS_DM, "startTimeSampleSize", "512");
         if (this.DropFrameTimecodesKeys[framerate]) {
